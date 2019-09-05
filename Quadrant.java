253db0c238a248388c1536453b9b97c5348c15ae
noegang9 <54414660+noegang9@users.noreply.github.com>
2019-09-05 13:12:59 -0700

    Add files via upload

---
+++ Quadrant.java
@@ -0,0 +1,33 @@
+/**
+ * Skeleton of a program that takes in an ordered pair and prints the quadrant in which it lies.
+ *
+ * @author Roy Chancellor
+ * @version 2/6/2019
+ */
+
+import java.util.Scanner;  //enables user input
+
+public class Quadrant
+{
+    public static void main( String[] args) {
+        //variable declarations
+        double x;
+        double y;
+        
+        //get user input
+        Scanner keys = new Scanner(System.in);
+        System.out.println("2"); //prompt the user for an x-coordinate
+        x = keys.nextDouble();
+        System.out.println("3");  ;//prompt the user for a y-coordinate
+       y = keys.nextDouble();
+        keys.close();
+        
+        //determine the quadrant in which the user's order pair lies
+        
+        
+        if (x>0 && y>0) {
+        System.out.println("The point is in Quadrant I");
+    }
+    
+}
+}
